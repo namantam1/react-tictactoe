05ca770c3ae6becb6020d22015be8a2f88cd102a
Naman Tamrakar <namantam1@gmail.com>
2020-09-09 11:36:12 +0530

    Updates

---
+++ precache-manifest.3f9fca86bea61b14c1b7170a3181d43c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad4d8e9323ef76a1758819998b2439c",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "b8ba3e101f27c129f075",
+    "url": "/react-tictactoe/static/css/main.2afb2ac7.chunk.css"
+  },
+  {
+    "revision": "87c9789d74ae77683bba",
+    "url": "/react-tictactoe/static/js/2.f55e4ebc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.f55e4ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ba3e101f27c129f075",
+    "url": "/react-tictactoe/static/js/main.6a0a4527.chunk.js"
+  },
+  {
+    "revision": "5128770873a6e07c8994",
+    "url": "/react-tictactoe/static/js/runtime-main.a450cb30.js"
+  }
+]);
